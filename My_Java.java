41862f15564ce9016fd138fae4efa568e942f3ac
alsgur3319 <alsgur3319>
2019-11-18 17:58:39 +0900

    Add My_Java [Add & Sub & Mul]

---
+++ My_Java.java
@@ -0,0 +1,12 @@
+class My_Java
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		int sub = a - b;
+		int mul = a * b;
+		System.out.println("Github ¿¬½À "+ "\n" + sum + sub);
+	}
+}
